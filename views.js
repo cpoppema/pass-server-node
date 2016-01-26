8470fd279af83001334029df8b0548b856357cf9
cpoppema <cpoppema@gmail.com>
2016-01-26 19:32:48 +0100

    Update .eslintrc with npm/node-specific styling and update bits and pieces of *.js

--- views.js
+++ views.js
@@ -1,46 +1,46 @@
-'use strict';
+'use strict'
 
-var store = require('./store');
+var store = require('./store')
 
 
 module.exports = function(router) {
   router.post('/secrets', function(req, res) {
 
     store.getList(function(secrets) {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify(secrets, null, 2));
-    });
-  });
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.end(JSON.stringify(secrets, null, 2))
+    })
+  })
 
   router.post('/secret', function(req, res) {
     // retrieve path + username from request
-    var relPath = req.body.path;
+    var relPath = req.body.path
     if (typeof relPath === typeof void 0) {
-      res.writeHead(400, {'Content-Type': 'application/json'});
+      res.writeHead(400, {'Content-Type': 'application/json'})
       res.end(JSON.stringify({
         error: 'Invalid secret requested.'
-      }, null, 2));
+      }, null, 2))
     }
-    var username = req.body.username;
+    var username = req.body.username
     if (typeof username === typeof void 0) {
-      res.writeHead(400, {'Content-Type': 'application/json'});
+      res.writeHead(400, {'Content-Type': 'application/json'})
       res.end(JSON.stringify({
         error: 'Invalid secret requested.'
-      }, null, 2));
+      }, null, 2))
     }
 
     store.getGpg(relPath, username, function(err, data) {
       if (!err) {
 
-        var pgpMessage = store.buildPgpMessage(data);
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(pgpMessage);
+        var pgpMessage = store.buildPgpMessage(data)
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end(pgpMessage)
       } else {
-        res.writeHead(err.errno, {'Content-Type': 'application/json'});
+        res.writeHead(err.errno, {'Content-Type': 'application/json'})
         res.end(JSON.stringify({
           error: err.message
-        }, null, 2));
+        }, null, 2))
       }
-    });
-  });
-};
+    })
+  })
+}
